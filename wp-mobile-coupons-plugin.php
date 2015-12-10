53bb68a15a0ef4cae4e9dd626481f10851c3e530
Coupon Wallet <dev@couponwallet.com>
2015-12-09 21:33:00 -0500

    Update wp-mobile-coupons-plugin.php

--- wp-mobile-coupons-plugin.php
+++ wp-mobile-coupons-plugin.php
@@ -11,3 +11,17 @@ License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: /coupons
 Text Domain: mobile-coupons
 */
+
+// Exit if accessed directly
+if( !defined('ABSPATH') ) {
+  exit;
+}
+
+function cw_register_mobile_coupon_post_type() {
+  $args = array(
+    'public'=>true,
+    'label'=>'Mobile Coupon'
+    );
+  register_post_type('mobile-coupon', $args);
+}
+add_action('init', 'cw_register_mobile_coupon_post_type');
