5fde9e340a5452a7ea3576e6afb73b62d3bff4cd
Coupon Wallet <dev@couponwallet.com>
2015-12-09 21:01:25 -0500

    Create wp-mobile-coupons-plugin.php

---
+++ wp-mobile-coupons-plugin.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: Mobile Coupons
+Plugin URI:  http://couponwallet.com
+Description: Add mobile coupon capabilities to your website
+Version:     0.1
+Author:      CouponWallet
+Author URI:  http://couponwallet.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Domain Path: /coupons
+Text Domain: mobile-coupons
+*/
